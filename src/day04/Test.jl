ca6692067086fbbfd9b2079ef259a5f9d7b8ed29
Jens Voß <Jens.Voss@wps.de>
2022-12-04 08:25:19 +0100

    Tag 4

--- src/day04/Test.jl
+++ src/day04/Test.jl
@@ -5,6 +5,6 @@ include("Today.jl")
 filename = string(@__DIR__, "/test.txt")
 
 @testset "Today" begin
-    @test part1(filename) == 0
-    @test part2(filename) == 0
+    @test part1(filename) == 2
+    @test part2(filename) == 4
 end
--- src/day04/Today.jl
+++ src/day04/Today.jl
@@ -5,6 +5,12 @@ function part1(filename)
     lines = readlines(f)
 
     score = 0
+    for line in lines
+        bounds = getBounds(line)
+        if (bounds[1][1] - bounds[2][1]) * (bounds[1][2] - bounds[2][2]) <= 0
+            score += 1
+        end
+    end
     score
 end
 
@@ -14,6 +20,17 @@ function part2(filename)
     lines = readlines(f)
 
     score = 0
+    for line in lines
+        bounds = getBounds(line)
+        if (length(intersect(bounds[1][1]:bounds[1][2], bounds[2][1]:bounds[2][2]))) > 0
+            score += 1
+        end
+    end
     score
 
+end
+
+function getBounds(line)
+    elves = split(line, ",")
+    [[parse(Int, b) for b in split(elves[i], "-")] for i in 1:2]
 end
