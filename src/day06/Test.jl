e2db42dc3f477a63bfba24ecf86946100078ed85
Jens Voß <Jens.Voss@wps.de>
2022-12-06 06:20:26 +0100

    Tag 6

--- src/day06/Test.jl
+++ src/day06/Test.jl
@@ -5,6 +5,6 @@ include("Today.jl")
 filename = string(@__DIR__, "/test.txt")
 
 @testset "Today" begin
-    @test part1(filename) == 0
-    @test part2(filename) == 0
+    @test part1(filename) == "7 5 6 10 11"
+    @test part2(filename) == "19 23 23 29 26"
 end
--- src/day06/Today.jl
+++ src/day06/Today.jl
@@ -1,19 +1,31 @@
-
 function part1(filename)
-
-    f = open(filename)
-    lines = readlines(f)
-
-    score = 0
-    score
+    solve(filename, 4)
 end
 
 function part2(filename)
+    solve(filename, 14)
+end
+
+function solve(filename, c)
 
     f = open(filename)
     lines = readlines(f)
 
-    score = 0
-    score
-
+    score = ""
+    for line in lines
+        for i = c:length(line)
+            ok = true
+            for j = i-c+1:i-1, k = j+1:i
+                if line[j] == line[k]
+                    ok = false
+                    break
+                end
+            end
+            if ok
+                score *= string(i) * " "
+                break
+            end
+        end
+    end
+    strip(score)
 end
