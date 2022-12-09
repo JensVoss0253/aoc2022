c9404cdeb91551ed63998387f565c69b4a718f48
Jens Voß <Jens.Voss@wps.de>
2022-12-09 06:51:02 +0100

    Tag 9 (leider noch nicht ganz korrekt)

--- src/day09/Test.jl
+++ src/day09/Test.jl
@@ -3,8 +3,10 @@ using Test
 include("Today.jl")
 
 filename = string(@__DIR__, "/test.txt")
+filename2 = string(@__DIR__, "/test2.txt")
 
 @testset "Today" begin
-    @test part1(filename) == 0
-    @test part2(filename) == 0
+    @test part1(filename) == 13
+    @test part2(filename) == 1
+    @test part2(filename2) == 36
 end
--- src/day09/Today.jl
+++ src/day09/Today.jl
@@ -1,19 +1,60 @@
 
 function part1(filename)
-
-    f = open(filename)
-    lines = readlines(f)
-
-    score = 0
-    score
+    solve(filename, 2)
 end
 
 function part2(filename)
+    solve(filename, 10)
+end
 
+function solve(filename, knotCount)
     f = open(filename)
     lines = readlines(f)
 
-    score = 0
-    score
+    knots = Array{Cell}(undef, knotCount)
+    for k = 1:knotCount
+        knots[k] = Cell(0, 0)
+    end
+    tails = Set()
 
+    for line in lines
+        direction = SubString(line, 1, 1)
+        count = parse(Int, SubString(line, 3))
+        for c = 1:count
+            knots[1] = move(knots[1], direction)
+            for k = 2:knotCount
+                knots[k] = follow(knots[k-1], knots[k])
+            end
+            push!(tails, knots[knotCount])
+        end
+    end
+
+    length(tails)
+end
+
+struct Cell
+    x::Int
+    y::Int
+end
+
+function move(cell::Cell, direction)::Cell
+    if direction == "R"
+        Cell(cell.x + 1, cell.y)
+    elseif direction == "L"
+        Cell(cell.x - 1, cell.y)
+    elseif direction == "U"
+        Cell(cell.x, cell.y + 1)
+    else
+        Cell(cell.x, cell.y - 1)
+    end
+end
+
+function follow(head::Cell, tail::Cell)::Cell
+    if abs(head.x - tail.x) == 2
+        Cell((head.x + tail.x) ÷ 2, head.y)
+    elseif abs(head.y - tail.y) == 2
+        Cell(head.x, (head.y + tail.y) ÷ 2)
+    else
+        tail
+    end
 end
